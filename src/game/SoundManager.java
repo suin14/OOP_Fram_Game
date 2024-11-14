729fdd156a2d1d35c1866f63037348708655ce6d
suin14 <sil0410@163.com>
2024-11-15 00:43:55 +0800

    播放音效

--- src/game/SoundManager.java
+++ src/game/SoundManager.java
@@ -7,20 +7,34 @@ import java.io.IOException;
 public class SoundManager {
     private static String path = System.getProperty("user.dir") + File.separator + "assets" + File.separator + "audio" + File.separator;
 
+    private Clip bgmClip;
+
+    private Clip sfxClip;
+
     // 播放背景音乐
     public void playBGM() {
         try {
             File bgmFile = new File(path + "bgm.wav");
-            AudioInputStream audioIn = AudioSystem.getAudioInputStream(bgmFile);
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioIn);
-
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-            clip.start();
+            AudioInputStream bgmStream = AudioSystem.getAudioInputStream(bgmFile);
+            bgmClip = AudioSystem.getClip();
+            bgmClip.open(bgmStream);
+            bgmClip.loop(Clip.LOOP_CONTINUOUSLY);
+            bgmClip.start();
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
     }
 
     // 播放声效
+    public void playSFX(String sfx) {
+        try {
+            File sfxFile = new File(path + sfx + ".wav");
+            AudioInputStream sfxStream = AudioSystem.getAudioInputStream(sfxFile);
+            sfxClip = AudioSystem.getClip();
+            sfxClip.open(sfxStream);
+            sfxClip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
 }
